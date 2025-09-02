3c33e690cf057af039c40c9600307c9cafcce817
turingcompletionist <coophan11@gmail.com>
2025-09-02 18:32:56 -0500

    file input support - untested

--- translator.py
+++ translator.py
@@ -8,15 +8,36 @@ def map_maker():
 
 tunic_map = map_maker()
 
-def parse(input):
+# parse tunic code into phonetic transcription
+def parse(input: str) -> str:
     if input in tunic_map.keys():
         return tunic_map[input]
     else:
         return "?"
+    
+# parse line of input (use "." as deliminator between each tunic character)
+def parse_line(input: str) -> list[str]:
+    word = []
+    split_input: list[str] = input.split(".")
+    for char in split_input:
+        if char in tunic_map.keys():
+            word.append(tunic_map[char])
+        else:
+            word.append("?")
+    return word
+    
+# parse file where each line is a tunic word
+def parse_file(filename: str) -> list[list[str]]:
+    with open(filename, "r") as f:
+        return [parse_line(line) for line in f.readlines()]
 
+# main repl
 while True:
-    split_input = input().split(".")
-    for a in split_input:
-        print(parse(a), end="")
-    print()
-
+    words: list[list[str]] = []
+    input_str = input()
+    if ".txt" in input_str:
+        words = parse_file(input_str)
+    else:
+        words.append(parse_line(input_str))
+    for word in words:
+        print(word)
